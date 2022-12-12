546258c1a0f77786e8375f574af2a1c4b77d352a
lorena tamayo <cottalorena@gmail.com>
2022-12-12 09:50:09 +0100

    asignar tarea

--- Ang_node/src/app/app.module.ts
+++ Ang_node/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TareasComponent } from "./tareas.component";
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-   
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,TareasComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        
+    ]
 })
 export class AppModule { }
---
+++ Ang_node/src/app/tareas.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tareas',
+  template: `Listado tareas:<div *ngFor="let tarea of tareas"></div>Tarea: {{tarea.trabajo}}, Usuario:{{tarea.usuario}}`,
+  
+})
+export class TareasComponent {
+    tarea:any;
+    
+    tareas=
+    [
+        {trabajo:'primera tarea', usuario:'david'},
+        {trabajo:'segunda tarea', usuario:'Daniel'}
+    ]
+
+ 
+}
